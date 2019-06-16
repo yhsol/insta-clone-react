308b0974b5fab035ff581c5afd6c4cdbf11de4a5
yhsol <yhsol1592@gmail.com>
2019-06-16 21:14:16 +0900

    add Input component

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,12 +1,36 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div`
+const Container = styled.input`
   background-color: ${props => props.theme.backgroundColor};
+  border: ${props => props.theme.boxBorder};
+  border-radius: ${props => props.theme.borderRadius};
+  padding: 0px 0.5rem;
+  font-size: 12px;
+  ${props => props.theme.whiteBox};
+  width: 60%;
+  margin: 0 auto;
+  height: 1.7rem;
 `;
 
-const Input = ({ children }) => {
-  return <Container>{children}</Container>;
+const Input = ({
+  placeholder,
+  required = true,
+  value,
+  onChange,
+  type = "text",
+  className
+}) => {
+  return (
+    <Container
+      placeholder={placeholder}
+      required={required}
+      value={value}
+      onChange={onChange}
+      type={type}
+      className={className}
+    />
+  );
 };
 
 export default Input;

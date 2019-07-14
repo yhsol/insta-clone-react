a0032e97edcffd22fe7bbab4067d8e3ba11c23db
yhsol <yhsol1592@gmail.com>
2019-07-14 15:13:18 +0900

    add comment

--- src/Components/Router/Post/PostPresenter.js
+++ src/Components/Router/Post/PostPresenter.js
@@ -179,7 +179,6 @@ const PostPresenter = ({
                   likeCount < 2 ? `${likeCount} like` : `${likeCount} likes`
                 }
               />
-<<<<<<< HEAD
               {comments &&
                 comments.map(comment => (
                   <div key={comment.id}>
@@ -187,9 +186,6 @@ const PostPresenter = ({
                     <span>{comment.text}</span>
                   </div>
                 ))}
-=======
-              <div>{newComment.value}</div>
->>>>>>> ad6f144381e3f7170cecadadd2b3c37ece63a8b1
               <TimeStamp>{now} days ago...</TimeStamp>
               <CommentForm>
                 {/* <CommentInput placeholder={"Comment!"} {...newComment} /> */}

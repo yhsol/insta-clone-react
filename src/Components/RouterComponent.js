72d95c9a828cdb51b85f7afcfc7f01eda89b8856
yhsol <yhsol1592@gmail.com>
2019-07-21 13:15:05 +0900

    edit using router

--- src/Components/RouterComponent.js
+++ src/Components/RouterComponent.js
@@ -1,35 +1,20 @@
 import React from "react";
-import { BrowserRouter as Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import PropTypes from "prop-types";
 import Feed from "./Router/Feed";
 import Auth from "./Router/Auth";
 import Profile from "./Router/Profile";
 import Notification from "./Router/Notification";
 import Explore from "./Router/Explore";
-import SearchContainer from "./Router/Search/SearchContainer";
+import Search from "./Router/Search";
 
 const LoggedInRoutes = () => (
   <Switch>
-    {/* <Route exact path="/" component={Feed} />
+    <Route exact path="/" component={Feed} />
     <Route path="/explore" component={Explore} />
-    <Route paht="/notification" component={Notification} />
+    <Route path="/notification" component={Notification} />
     <Route path="/search" component={Search} />
-    <Route path="/:username" component={Profile} /> */}
-    <Route exact path="/">
-      <Feed />
-    </Route>
-    <Route path="/explore">
-      <Explore />
-    </Route>
-    <Route path="/notification">
-      <Notification />
-    </Route>
-    <Route path="/search">
-      <SearchContainer />
-    </Route>
-    <Route path="/:username">
-      <Profile />
-    </Route>
+    <Route path="/:username" component={Profile} />
   </Switch>
 );
 

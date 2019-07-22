0cad8cc7333872dcfc13f26cbc775114aadbf5c7
yhsol <yhsol1592@gmail.com>
2019-07-22 21:17:41 +0900

    add profile section

--- src/Components/Router/Profile.js
+++ src/Components/Router/Profile.js
@@ -1,5 +1,35 @@
 import React from "react";
 import styled from "styled-components";
+import { gql } from "apollo-boost";
+import { withRouter } from "react-router-dom";
+import { useQuery } from "react-apollo-hooks";
+import Loader from "../Loader";
+import Avatar from "../Avatar";
+
+const GET_USER = gql`
+  query seeUser($username: String!) {
+    seeUser(username: $username) {
+      id
+      avatar
+      username
+      fullname
+      amIFollowing
+      itsMe
+      bio
+      followingCount
+      followersCount
+      postsCount
+      posts {
+        id
+        files {
+          url
+        }
+        likeCount
+        commentCount
+      }
+    }
+  }
+`;
 
 const Wrapper = styled.div`
   min-height: 80vh;
@@ -10,8 +40,46 @@ const Wrapper = styled.div`
   font-size: 14px;
 `;
 
-const Profile = () => {
-  return <Wrapper>profile</Wrapper>;
+const Header = styled.header``;
+
+const Profile = ({
+  match: {
+    params: { username }
+  }
+}) => {
+  const { loading, data } = useQuery(GET_USER, { variables: { username } });
+  console.log(data);
+
+  if (loading === true) {
+    return (
+      <Wrapper>
+        <Loader />
+      </Wrapper>
+    );
+  } else {
+    const {
+      seeUser: {
+        id,
+        avatar,
+        fullname,
+        amIFollowing,
+        itsMe,
+        bio,
+        followingCount,
+        followersCount,
+        postsCount,
+        posts
+      }
+    } = data;
+    return (
+      <Wrapper>
+        <Avatar size={"lg"} url={avatar} />
+      </Wrapper>
+    );
+  }
+  // <Wrapper>
+  //   {loading ? <Loader /> : <Avatar size={"lg"} url={avatar} />}
+  // </Wrapper>
 };
 
-export default Profile;
+export default withRouter(Profile);
